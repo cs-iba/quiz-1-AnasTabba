d779d5668e4082541bc2163017798c782c9bfbc8
AnasTabba <anastex1@gmail.com>
2024-09-16 12:56:17 +0500

    this is my code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,21 +8,39 @@ using namespace std;
 // `pos` specifies the index to sort by if sorting a vector of pairs.
 template <typename T>
 void stableSelectionSort(vector<T>& a) {
-    
+    int n = a.size();
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
+        }
+        // Move the minimum element to its correct position
+        T minElement = a[min];
+        while (min > i) {
+            a[min] = a[min - 1];
+            min--;
+        }
+        a[i] = minElement;
+    }
 }
 
 // Unstable Selection Sort given in slides
 template <typename T>
 void selectionSort(vector<T>& a) {
-     int n = a.size();
-        for (int i = 0; i < n; i++) {
-            int min = i;
-            for (int j = i+1; j < n; j++) {
-                if (a[j] < a[min]) min = j;
+    int n = a.size();
+    for (int i = 0; i < n; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (a[j] < a[min]) {
+                min = j;
             }
-            std::swap(a[i], a[min]);
         }
+        swap(a[i], a[min]);
+    }
 }
+
 int main() {
     // Example usage with an array of pairs
     vector<pair<int, int>> arr = {{3, 1}, {2, 2}, {1, 3}, {2, 1}};
@@ -33,11 +52,11 @@ int main() {
     }
     cout << endl;
 
-    // Sort by the second element of the pairs
-    selectionSort(arr);
+    // Sort by the second element of the pairs using stable selection sort
+    stableSelectionSort(arr);
 
     // Print sorted array
-    cout << "Sorted array by the first element:" << endl;
+    cout << "Sorted array by the first element using stable selection sort:" << endl;
     for (const auto& p : arr) {
         cout << "(" << p.first << ", " << p.second << ") ";
     }
